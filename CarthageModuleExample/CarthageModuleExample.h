e782cb06ffcdc08de330e2ae0830d9a34fbe9cdf
Doba Duc <duc.doba@linecorp.com>
2015-07-02 18:41:14 +0900

    Initial Commit

---
+++ CarthageModuleExample/CarthageModuleExample.h
@@ -0,0 +1,19 @@
+//
+//  CarthageModuleExample.h
+//  CarthageModuleExample
+//
+//  Created by Doba Duc on 7/2/15.
+//  Copyright (c) 2015 Doba Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageModuleExample.
+FOUNDATION_EXPORT double CarthageModuleExampleVersionNumber;
+
+//! Project version string for CarthageModuleExample.
+FOUNDATION_EXPORT const unsigned char CarthageModuleExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageModuleExample/PublicHeader.h>
+
+
