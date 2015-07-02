a81324b5f71d797d1af09fd740983397a2a33577
Doba Duc <duc.doba@linecorp.com>
2015-07-02 19:14:42 +0900

    Initial commit

---
+++ CarthageModuleExample/Core.swift
@@ -0,0 +1,18 @@
+//
+//  Core.swift
+//  CarthageModuleExample
+//
+//  Created by Doba Duc on 7/2/15.
+//  Copyright (c) 2015 Doba Duc. All rights reserved.
+//
+
+class Core {
+    var name: String
+
+    init(name personName: String) {
+        name = personName
+    }
+
+    func sayHi(to person: String) {
+    }
+}
